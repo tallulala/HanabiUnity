064ef741529d750faf79d513170a0a90619a66b8
Eve <egottwald@mills.edu>
2018-06-18 15:42:34 -0400

    Start button task #2

--- Hanabi/Assets/Scenes/Controllers/ButtonController.cs
+++ Hanabi/Assets/Scenes/Controllers/ButtonController.cs
@@ -5,15 +5,40 @@ using UnityEngine.UI;
 
 public class ButtonController : MonoBehaviour {
 
+    public GameObject Card;
     public GameObject[] cards;
-    public Vector3[] myhand;
 
-	public void OnClick()
+    //public Transform faceUp;
+
+    public Vector3 deckPos;
+    public Vector3 offScreen;
+
+    public Vector3[] playerHand;
+    public Vector3[] computerHand;
+
+    public void Start()
+    {
+        cards = new GameObject[50];
+        deckPos = new Vector3(-20f, 0f, 6f);
+        //faceUp.rotation = Quaternion.Euler(180, 0, 180);
+        offScreen = new Vector3(6000000f, 0f, 6000000f);
+
+    }
+
+    public void OnClick()
 	{
-		gameObject.SetActive (false);
+        gameObject.transform.position = offScreen;
+
+        for (int i = 0; i < 50; i++)
+        {
+            cards[i] = Instantiate(Card, deckPos, Quaternion.identity);
+        }
         
-		//GameObject.FindGameObjectWithTag ("Start Button").GetComponent<Button>().enabled = false;
-		Debug.Log ("button");
+        // Deal();
 	}
 		
+    public void Deal ()
+    {
+        //if any empty spaces new card
+    }
 }
