842bd3b5aab27ffc3bde8636afa28391ddf3d21a
Eve <egottwald@mills.edu>
2018-06-21 14:26:25 -0400

    Deal method
    Now we're cookin w gassss

--- Hanabi/Assets/Scenes/Controllers/ButtonController.cs
+++ Hanabi/Assets/Scenes/Controllers/ButtonController.cs
@@ -7,15 +7,12 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour {
 
     public GameObject Card;
-    public GameObject PlayField;
-    public GameObject Background;
+
     public GameObject[] Deck;
 
-    public int DeckIndex;
+    public int DeckCount;
 
     public Vector3 DeckPos;
-    public Vector3 PlayFieldSize;
-    public Vector3 BackgroundSize;
     public Vector3 OffScreen;
 
     public Vector3[] PlayerHand;
@@ -23,15 +20,14 @@ public class ButtonController : MonoBehaviour {
     public Vector3[] PlayArea;
     public Vector3[] DiscardPile;
 
-
     public void Start()
     {
-        Background.transform.Translate(new Vector3(0f, 0f, 0f), Space.World);
-        PlayField.transform.Translate(new Vector3(0, 0.01f, 0), Space.World);
-       
+        Card = CardController.FindObjectOfType<GameObject>();
+
+        DeckPos = new Vector3(-1638, 1, 345);
+
         Deck = new GameObject[50];
-        DeckIndex = 0;
-        DeckPos = new Vector3(-87, 1, 50);
+        DeckCount = 0;
 
         OffScreen = new Vector3(6000000f, 0f, 6000000f);
 
@@ -39,8 +35,8 @@ public class ButtonController : MonoBehaviour {
         PlayerHand = new Vector3[5];
         for (int i = 0; i < 5; i++)
         {
-            ComputerHand[i] = new Vector3((12 * i), 1, 40);
-            PlayerHand[i] = new Vector3((12 * i), 1, -40);
+            ComputerHand[i] = new Vector3((1000 + (330 * i)), 2, 200);
+            PlayerHand[i] = new Vector3((1000 + (330 * i)), 2, -1500);
         }
     }
 
@@ -48,46 +44,51 @@ public class ButtonController : MonoBehaviour {
     {
         gameObject.transform.position = OffScreen;
 
+        GenerateDeck();
+
+        Shuffle(Deck);
+
+        Deal(PlayerHand);
+        Debug.Log("player dealt");
+
+        Deal(ComputerHand);
+        Debug.Log("Comp dealt");
+    }
+
+    public void GenerateDeck()
+    {
         for (int i = 0; i < 50; i++)
         {
             Deck[i] = Instantiate(Card, DeckPos, Quaternion.identity);
             Deck[i].name = ("Card" + i);
-            DeckIndex++;
-        }
-
-        Deal();
+            Deck[i].transform.Rotate(Vector3.right, 180);
+            DeckCount++;
+        }        
     }
-    
-    public void Deal ()
+
+    public void Deal (Vector3[] hand)
     {
         Vector3 position;
         float radius = 1;
- 
-        foreach (Vector3 pos in ComputerHand) 
+
+        foreach (Vector3 pos in hand)
         {
             position = pos;
 
             if (Physics.CheckSphere(position, radius))
             {
-                Deck[DeckIndex - 1].transform.Translate(position, Space.World);
-                Deck[DeckIndex - 1].transform.Rotate(Vector3.up, 180);
-                DeckIndex--;
+                Deck[DeckCount - 1].transform.Translate(position, Space.World);
+                Deck[DeckCount - 1].transform.Rotate(Vector3.right, 180);
+                DeckCount--;
             }
         }
+    }
 
-        foreach (Vector3 pos in PlayerHand)
-        {
-            position = pos;
+    public GameObject[] Shuffle(GameObject[] deck)
+    {
+        // TODO: write method
 
-            if (Physics.CheckSphere(position, radius))
-            {
-                Deck[DeckIndex - 1].transform.Translate(position, Space.World);
-                Deck[DeckIndex - 1].transform.Rotate(Vector3.up, 180);
-                DeckIndex--;
-            }
-        }
-
-        Debug.Log("Dealt");
+        return deck;
     }
 }
 
--- Hanabi/Assets/Scenes/Controllers/CardController.cs
+++ Hanabi/Assets/Scenes/Controllers/CardController.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CardController : MonoBehaviour {
+public class CardController : MonoBehaviour
+{
+    public GameObject Card;
 
-    
+    Color color;
+    Rank rank;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    enum Rank : int
+    {
+        one = 1,
+        two = 2,
+        three = 3,
+        four = 4,
+        five = 5
+    };
+
+    enum Color
+    {
+        white, blue, yellow, red, green
+    };
+
+    private void Start()
+    {
+        Card = gameObject;
+        Card.SetActive(true);
+    }
+
+    public void setColor(int toColor)
+    {
+        color = (Color) toColor;
+    }
+
+    public void setRank(int toRank)
+    {
+        rank = (Rank) toRank;
+    }
 }
